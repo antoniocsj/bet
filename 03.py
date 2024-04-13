b2ce695c986a95be3722e4de808d24bc23d5015b
Antonio CSJ <acsj369@gmail.com>
2024-04-13 12:31:25 -0300

    ---

--- 03.py
+++ 03.py
@@ -1,5 +1,6 @@
 import os
 import json
+import sqlite3
 
 
 def read_json(filename: str) -> dict:
@@ -55,5 +56,43 @@ def teste_01():
     pass
 
 
+def create_database(file_json_in: str, file_sqlite):
+    data = read_json(file_json_in)
+    apostas = data['apostas']
+
+    sqliteConnection = None
+    try:
+        # Connect to DB and create a cursor
+        sqliteConnection = sqlite3.connect(file_sqlite)
+        cursor = sqliteConnection.cursor()
+        print('DB Init')
+
+        # Write a query and execute it with cursor
+        query = 'select sqlite_version();'
+        cursor.execute(query)
+
+        # Fetch and output result
+        result = cursor.fetchall()
+        print('SQLite Version is {}'.format(result))
+
+        # Close the cursor
+        cursor.close()
+
+    # Handle errors
+    except sqlite3.Error as error:
+        print('Error occurred - ', error)
+
+    # Close DB Connection irrespective of success
+    # or failure
+    finally:
+        if sqliteConnection:
+            sqliteConnection.close()
+            print('SQLite Connection closed')
+
+
+def teste_02():
+    create_database('apostas.json', 'database.db')
+
+
 if __name__ == '__main__':
-    teste_01()
+    teste_02()
